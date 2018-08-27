106c96bec481bd95ccf66c6170e0ecf8744c0bd5
VadimZverev <39337335+VadimZverev@users.noreply.github.com>
2018-08-27 15:11:42 +0400

    Используем фильтр OutputCache

---
+++ Filtres/Controllers/SelectiveCacheController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace Filtres.Controllers
+{
+    public class SelectiveCacheController : Controller
+    {
+        public ActionResult Index()
+        {
+            Response.Write("Метод действия звпущен: " + DateTime.Now);
+            return View();
+        }
+
+        [OutputCache(Duration = 30)]
+        public ActionResult ChildAction()
+        {
+            Response.Write("дочерний метод действия звпущен: " + DateTime.Now);
+            return View();
+        }
+    }
+}
