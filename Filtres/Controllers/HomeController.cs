44bc433750644a6a3cce2910823866a6efaadf05
VadimZverev <39337335+VadimZverev@users.noreply.github.com>
2018-08-27 12:39:13 +0400

    Фильтрация без атрибутов

--- Filtres/Controllers/HomeController.cs
+++ Filtres/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Filtres.Infrastructure;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,6 +10,8 @@ namespace Filtres.Controllers
 {
     public class HomeController : Controller
     {
+        private Stopwatch timer;
+
         //[CustomAuth(false)]
         [Authorize(Users = "admin")]
         public string Index()
@@ -37,12 +40,21 @@ namespace Filtres.Controllers
             }
         }
 
-        [ProfileAction]
-        [ProfileResult]
-        [ProfileAll]
         public string FilterTest()
         {
             return "Это FilterTest действие";
+        }
+
+        protected override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            timer = Stopwatch.StartNew();
+        }
+
+        protected override void OnResultExecuted(ResultExecutedContext filterContext)
+        {
+            timer.Stop();
+            filterContext.HttpContext.Response.Write(
+                    string.Format($"<div>Общее время: {timer.Elapsed.TotalSeconds}</div>"));
         }
     }
 }
