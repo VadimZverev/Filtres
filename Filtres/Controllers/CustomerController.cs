6fa724ebc0aa8f78f62ced32daed7bb88e75a437
VadimZverev <39337335+VadimZverev@users.noreply.github.com>
2018-08-27 12:56:00 +0400

    Используем глобальные фильтры

---
+++ Filtres/Controllers/CustomerController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Filtres.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public string Index()
+        {
+            return "Это Customer контроллер";
+        }
+    }
+}
