4150db1780b1e73d8c9944792b5e9b048d642e6f
VadimZverev <39337335+VadimZverev@users.noreply.github.com>
2018-08-27 13:15:15 +0400

    Задаем порядок выполнения фильтров

--- Filtres/Controllers/CustomerController.cs
+++ Filtres/Controllers/CustomerController.cs
@@ -1,9 +1,12 @@
-﻿using System.Web.Mvc;
+﻿using Filtres.Infrastructure;
+using System.Web.Mvc;
 
 namespace Filtres.Controllers
 {
     public class CustomerController : Controller
     {
+        [SimpleMessage(Message = "A", Order = 2)]
+        [SimpleMessage(Message = "B", Order = 1)]
         public string Index()
         {
             return "Это Customer контроллер";
---
+++ Filtres/Infrastructure/SimpleMessageAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Filtres.Infrastructure
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+    public class SimpleMessageAttribute : FilterAttribute, IActionFilter
+    {
+        public string Message;
+
+        public void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            filterContext.HttpContext.Response.Write(
+                string.Format($"<div>[После действия: {Message}]<div>"));
+        }
+
+        public void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            filterContext.HttpContext.Response.Write(
+                string.Format($"<div>[Перед действием: {Message}]<div>"));
+        }
+    }
+}
